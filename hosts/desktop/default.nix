fd1b601219218f89857e4b33a876cbfd31df13ca
FloncDev <37045958+FloncDev@users.noreply.github.com>
2025-04-27 16:42:34 +0100

    fix: nixos config now works (hopefully)

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -3,16 +3,22 @@
   imports = [
     inputs.home-manager.nixosModules.home-manager
     ./hardware-configuration.nix
-    ./../../modules/nixos
-    ./../../modules/core
+    ../../modules/nixos
+    ../../modules/core
+    # ./../../modules/nixos
+    # ./../../modules/core
   ];
 
-  home-manager = {
-    useUserPackages = true;
-    useGlobalPkgs = true;
-    extraSpecialArgs = { inherit inputs username hosts; };
-    users.${username} = {
-      imports = [ ../home ];
-    };
-  };
+  # home-manager = {
+  #   useUserPackages = true;
+  #   useGlobalPkgs = true;
+  #   extraSpecialArgs = { inherit inputs username host; };
+  #   users.${username} = {
+  #     imports = [
+  #       # ../../modules/nixos
+  #       ../../modules/nixos/home.nix
+  #       ../../modules/core
+  #     ];
+  #   };
+  # };
 }
