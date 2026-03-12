02476806df2c08586b19ec1b5ad45ee1a32425ab
FloncDev <37045958+FloncDev@users.noreply.github.com>
2026-03-12 23:01:29 +0000

    feat: add hardware module
    Will need to be tested on desktop once it's added

---
+++ modules/system/optional/hardware.nix
@@ -0,0 +1,38 @@
+# TODO: Maybe refactor this into per-host?
+# So far I'll only be using everything here for desktop anyway
+{ ... }:
+{
+  flake.modules.nixos.hardware =
+    { pkgs, ... }:
+    {
+      hardware = {
+        graphics = {
+          enable = true;
+          extraPackages = [ pkgs.mesa ];
+        };
+
+        nvidia = {
+          modesetting.enable = true;
+          open = false;
+          nvidiaSettings = true;
+        };
+
+        bluetooth = {
+          enable = true;
+          settings = {
+            General = {
+              Experimental = true;
+              FastConnectable = true;
+              # Used for some airpods magic
+              # DeviceId = "bluetooth:004C:0000:0000";
+            };
+
+            Policy = {
+              AutoEnable = "true";
+            };
+          };
+        };
+        usb-modeswitch.enable = true;
+      };
+    };
+}
