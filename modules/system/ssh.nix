9dcc421657cdb18a0b0e6e03abfc36e6970ab910
FloncDev <37045958+FloncDev@users.noreply.github.com>
2026-03-12 23:58:13 +0000

    feat: improve ssh config with aliases and fail2ban

--- modules/system/ssh.nix
+++ modules/system/ssh.nix
@@ -2,6 +2,27 @@
 {
   # TODO: Add ssh-config here
   flake.modules.nixos.ssh = {
-    services.openssh.enable = true;
+    services = {
+      openssh.enable = true;
+      # SSH Should hopefully never be exposed, but just in case ;)
+      fail2ban.enable = true;
+    };
+
+    programs.ssh = {
+      # TODO: Better naming scheme for local vs tailscale
+      extraConfig = "
+        Host localdesktop
+          HostName 192.168.0.2
+          User flonc
+
+        Host localdenshi
+          HostName 192.168.0.3
+          User flonc
+
+        Host localhome
+          HostName 192.168.0.4
+          User flonc
+      ";
+    };
   };
 }
