c0b9229f5473b59da00a9832df4f7f3168eed80a
FloncDev <37045958+FloncDev@users.noreply.github.com>
2026-03-22 23:54:02 +0000

    fix: correct monitor names

--- modules/hosts/desktop/hyprland.nix
+++ modules/hosts/desktop/hyprland.nix
@@ -11,14 +11,13 @@
       settings = {
         monitor = [
           "DP-3, 2560x1440@165, 1080x480, 1"
-          # "DP-2, 1920x1080, 0x1080, 1"
           "HDMI-A-1, 1920x1080, 0x0, 1, transform, 1"
         ];
 
         workspace = [
-          "1, monitor:DP-1"
+          "1, monitor:DP-3"
           # TODO: Make games go to this workspace
-          "name:hames, monitor:DP-1, shadow:false, decorate:false, rounding:false, border:false, gapsout:0"
+          "name:games, monitor:DP-1, shadow:false, decorate:false, rounding:false, border:false, gapsout:0"
         ];
 
         # TODO: Some more of these
@@ -26,6 +25,6 @@
     };
 
     # Have bar only on second monitor
-    programs.waybar.settings.secondaryBar.output = [ "HDMI-A-1 "];
+    programs.waybar.settings.secondaryBar.output = [ "HDMI-A-1" ];
   };
 }
