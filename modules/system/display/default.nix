30a704356e52061c6a6b326c20d16b83338d85c9
FloncDev <37045958+FloncDev@users.noreply.github.com>
2026-03-23 00:16:54 +0000

    feat: add new cjk font

--- modules/system/display/default.nix
+++ modules/system/display/default.nix
@@ -1,33 +1,40 @@
 # Shared config for all systems with a desktop environment.
 { ... }:
 {
-  flake.modules.nixos.default-display = {
-    services = {
-      pipewire = {
-        enable = true;
-        alsa.enable = true;
-        alsa.support32Bit = true;
-        pulse.enable = true;
-        # TODO: Check why false and what jack does
-        jack.enable = false;
-        wireplumber.enable = true;
+  flake.modules.nixos.default-display =
+    { pkgs, ... }:
+    {
+      services = {
+        pipewire = {
+          enable = true;
+          alsa.enable = true;
+          alsa.support32Bit = true;
+          pulse.enable = true;
+          # TODO: Check why false and what jack does
+          jack.enable = false;
+          wireplumber.enable = true;
+        };
+
+        # Maybe move this into desktop
+        libinput.mouse = {
+          accelProfile = "flat";
+          accelSpeed = "-2.8";
+        };
+
+        xserver = {
+          enable = false;
+          xkb.layout = "us";
+
+          videoDrivers = [ "nvidia" ];
+        };
+
+        dbus.enable = true;
+        gnome.gnome-keyring.enable = true;
       };
 
-      # Maybe move this into desktop
-      libinput.mouse = {
-        accelProfile = "flat";
-        accelSpeed = "-2.8";
-      };
-
-      xserver = {
-        enable = false;
-        xkb.layout = "us";
-
-        videoDrivers = [ "nvidia" ];
-      };
-
-      dbus.enable = true;
-      gnome.gnome-keyring.enable = true;
+      environment.systemPackages = with pkgs; [
+        # Better cjk font
+        noto-fonts-cjk-sans
+      ];
     };
-  };
 }
