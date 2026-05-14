110523615e49508a2bf538ee07c8dddf4a50d21d
FloncDev <37045958+FloncDev@users.noreply.github.com>
2026-05-14 19:50:11 +0100

    feat: set firefox as the default browser

--- modules/users/flonc.nix
+++ modules/users/flonc.nix
@@ -46,6 +46,17 @@
           # TODO Have GPG key saved with nix config, also this will need to change per-device
           # signing.key = "C4BB780AFDE394ED";
         };
+
+        xdg.mimeApps = {
+          enable = true;
+          defaultApplications = {
+            "text/html" = "firefox.desktop";
+            "x-scheme-handler/http" = "firefox.desktop";
+            "x-scheme-handler/https" = "firefox.desktop";
+            "x-scheme-handler/about" = "firefox.desktop";
+            "x-scheme-handle/unknown" = "firefox.desktop";
+          };
+        };
       };
     }
   ];
