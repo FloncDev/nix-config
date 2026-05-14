f785c5b08db1c777329bbd4e3ee963791ebadb3b
FloncDev <37045958+FloncDev@users.noreply.github.com>
2026-05-14 19:03:40 +0100

    chore: formatting

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -1,19 +1,23 @@
 { inputs, ... }:
 {
-  flake.modules.nixos.default = {
-    imports = with inputs.self.modules.nixos; [
-      minimal
-      home-manager
+  flake.modules.nixos.default =
+    { lib, ... }:
+    {
+      imports = with inputs.self.modules.nixos; [
+        minimal
+        home-manager
 
-      # Under modules/system/
-      networking
-      ssh
-      security
-    ];
+        # Under modules/system/
+        networking
+        ssh
+        security
+      ];
 
-    virtualisation.docker.enable = true;
-    i18n.defaultLocale = "en_GB.UTF-8";
-  };
+      services.automatic-timezoned.enable = lib.mkDefault true;
+      programs.nix-ld.enable = true;
+      virtualisation.docker.enable = true;
+      i18n.defaultLocale = "en_GB.UTF-8";
+    };
 
   flake.modules.homeManager.default = {
     imports = with inputs.self.modules.homeManager; [
