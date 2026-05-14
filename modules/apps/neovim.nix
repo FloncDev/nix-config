dd8960108007472010e908732aa4173fdb5a6d6a
FloncDev <37045958+FloncDev@users.noreply.github.com>
2026-05-15 00:25:03 +0100

    chore: fix neovim warnings

--- modules/apps/neovim.nix
+++ modules/apps/neovim.nix
@@ -1,12 +1,14 @@
 # TODO: Use something like nixvim? Also setup an actual vim config :)
 {
   flake.modules.homeManager.neovim =
-    { pkgs, ... }:
+    { ... }:
     {
       programs.neovim = {
         enable = true;
         vimAlias = true;
         withNodeJs = true;
+        withRuby = false;
+        withPython3 = false;
       };
 
       # home.packages = with pkgs; [
