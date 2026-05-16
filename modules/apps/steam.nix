4a0d5d79e9766c263aca789aa3f45e7ae204a8cd
FloncDev <37045958+FloncDev@users.noreply.github.com>
2026-05-16 19:28:57 +0100

    feat: add mangohud and gamescope

--- modules/apps/steam.nix
+++ modules/apps/steam.nix
@@ -1,8 +1,10 @@
-{ ... }:
+{ inputs, ... }:
 {
   flake.modules.nixos.steam =
     { pkgs, ... }:
     {
+      home-manager.sharedModules = [ inputs.self.modules.homeManager.steam ];
+
       programs.steam = {
         enable = true;
         remotePlay.openFirewall = true;
@@ -11,10 +13,16 @@
         protontricks.enable = true;
       };
 
+      programs.gamescope.enable = true;
+
       environment.systemPackages = with pkgs; [
         protonup-qt
         # TODO: Try out protonplus instead and pick the better option
         protonplus
       ];
     };
+
+  flake.modules.homeManager.steam = {
+    programs.mangohud.enable = true;
+  };
 }
