204a5101dd067637ab7ecc4e349e054dbba1d057
FloncDev <37045958+FloncDev@users.noreply.github.com>
2026-05-16 16:28:04 +0100

    feat: enable swap

--- modules/hosts/desktop/desktop.nix
+++ modules/hosts/desktop/desktop.nix
@@ -30,11 +30,20 @@
       boot.kernelParams = [
         "video=DP-3:2560x1440@60"
         # "video=HDMI-A-1:e"
+
+        "zswap.enabled=1"
       ];
 
       # Fix for bluetooth dongle
       services.udev.extraRules = ''
         	  ATTR{idVendor}=="0bda", ATTR{idProduct}=="1a2b", RUN+="${lib.getExe pkgs.usb-modeswitch} -K -v 0bda -p 1a2b"
         	'';
+
+      # Enable swap
+      swapDevices = [
+        {
+          device = "/dev/disk/by-uuid/df7048a5-a4b9-427c-a22d-b95f4f9f9edd";
+        }
+      ];
     };
 }
