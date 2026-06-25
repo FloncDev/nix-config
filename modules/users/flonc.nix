c7a31a7841adb444e07bee94798c00352fc89b35
FloncDev <37045958+FloncDev@users.noreply.github.com>
2026-06-25 21:04:41 +0100

    fix: MIME type type

--- modules/users/flonc.nix
+++ modules/users/flonc.nix
@@ -54,7 +54,7 @@
             "x-scheme-handler/http" = "firefox.desktop";
             "x-scheme-handler/https" = "firefox.desktop";
             "x-scheme-handler/about" = "firefox.desktop";
-            "x-scheme-handle/unknown" = "firefox.desktop";
+            "x-scheme-handler/unknown" = "firefox.desktop";
           };
         };
       };
