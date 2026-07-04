a87415c330071758cd91f7e35d76ddd29b01821b
FloncDev <37045958+FloncDev@users.noreply.github.com>
2026-07-04 18:11:27 +0100

    feat: initial darwin configuration

--- modules/hosts/darwin/darwin.nix
+++ modules/hosts/darwin/darwin.nix
@@ -3,6 +3,12 @@
   flake.darwinConfigurations = inputs.self.lib.mkDarwin "aarch64-darwin" "darwin";
 
   flake.modules.darwin.darwin = {
+    imports = with inputs.self.modules.darwin; [
+      default
+      flonc
+      default-apps
+    ];
 
+    networking.hostName = "darwin";
   };
 }
